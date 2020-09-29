93017a3d60af89f88edf5d82f0e32a51d2e34c42
SCORP\Scorpion <omarshherif@yahoo.com>
2020-09-29 07:10:34 +0200

    CartScreen

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/widgets.dart';
 import 'package:furniture_app/screens/Welcome/welcome_screen.dart';
-//import 'package:furniture_app/screens/cart/cart_screen.dart';
+import 'package:furniture_app/screens/cart/cart_screen.dart';
 import 'package:furniture_app/screens/details/details_screen.dart';
 //import 'package:shop_app/screens/forgot_password/forgot_password_screen.dart';
 import 'package:furniture_app/screens/home/home_screen.dart';
@@ -17,5 +17,5 @@ final Map<String, WidgetBuilder> routes = {
   OtpScreen.routeName: (context) => OtpScreen(),
   HomeScreen.routeName: (context) => HomeScreen(),
   DetailsScreen.routeName: (context) => DetailsScreen(),
-  //CartScreen.routeName: (context) => CartScreen(),
+  CartScreen.routeName: (context) => CartScreen(),
 };
--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,8 +1,12 @@
+//import 'dart:js';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:furniture_app/constants.dart';
 import 'package:furniture_app/screens/home/components/body.dart';
 import 'package:furniture_app/size_config.dart';
+import 'package:furniture_app/screens/cart/cart_screen.dart';
+import 'package:furniture_app/routes.dart';
 
 class HomeScreen extends StatelessWidget {
   static String routeName = "/home";
@@ -19,12 +23,12 @@ class HomeScreen extends StatelessWidget {
   AppBar buildAppBar() {
     return AppBar(
       leading: IconButton(
-        icon: SvgPicture.asset(
-          "assets/icons/menu.svg",
-          height: SizeConfig.defaultSize * 2, //20
-        ),
-        onPressed: () {},
-      ),
+          icon: SvgPicture.asset(
+            "assets/icons/menu.svg",
+            height: SizeConfig.defaultSize * 2, //20
+          ),
+          onPressed: () {} //Navigator.pushNamed(context, CartScreen.routeName),
+          ),
       actions: <Widget>[
         IconButton(
           icon: SvgPicture.asset(
